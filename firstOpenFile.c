8b14f1880563e96b33c12d06ed5fa724a0cfdc26
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-06-11 23:57:21 -0400

    Updated firstOpenFile where i have added a dynamic file generator as well

--- firstOpenFile.c
+++ firstOpenFile.c
@@ -6,6 +6,21 @@ int main()
 {
     FILE *fileOpen;
     fileOpen = fopen("firstOpenFile.txt", "w");
+    printf("Do you want to create a new File??\n");
+    printf("Press Y for Yes and N for No: ");
+    char query;
+    scanf("%c", &query);
+    if (query == 'Y' || query == 'y')
+    {
+        printf("What is the name of the File\n");
+        char fileName[100];
+        scanf("%s", fileName);
+        fileOpen = fopen(fileName, "w");
+    }
+    else
+    {
+        printf("File is not created\ns");
+    }
     if (fileOpen == NULL)
     {
         printf("File not found: %s\n", strerror(errno));
